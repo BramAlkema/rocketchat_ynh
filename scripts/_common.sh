5fc61e24decb1706e021253294ddd45d59247696
BramAlkema <info@bramalkema.nl>
2020-03-23 03:46:31 +0100

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -34,7 +34,7 @@ waitforservice() {
 
 installnode(){
   if [ $DEBIAN_ISSUE == "stretch" ]; then
-    sudo curl -sL https://deb.nodesource.com/setup_8.x -o nodesource_setup.sh
+    sudo curl -sL https://deb.nodesource.com/setup_12.x -o nodesource_setup.sh
     sudo bash nodesource_setup.sh
     sudo apt-get install -y nodejs
   else
